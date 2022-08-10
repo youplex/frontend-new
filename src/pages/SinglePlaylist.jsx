700a17231f92a0c2ee81dfc92318f63641379269
Prajna1999 <gituprajna20@mail.com>
2022-08-10 18:07:18 +0530

    Add card component

--- src/pages/SinglePlaylist.jsx
+++ src/pages/SinglePlaylist.jsx
@@ -4,6 +4,8 @@ import {Sidebar} from '../components';
 import {Playlist} from '../components';
 import React from 'react';
 import {Link} from 'react-router-dom';
+import  Card  from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
 
 function SinglePlaylist() {
   return (
@@ -11,17 +13,30 @@ function SinglePlaylist() {
         <Sidebar />
         <Navbar page="Your Playlists" />
 
-        <div className="flex w-4/5 bg-blue-500 ">
+        <div className=" relative flex bg-blue-500 ">
+          <div>
+
             <Playlist   />
-            <div className="">
+          </div>
+            <div className="absolute right-52 top-4">
                 <h1>Crework BWU</h1>
                 <p>This is a playlist related to BWU projects built by <br></br> students of different batches.</p>
-                <Link to="#">
+                <div className="bg-primary w-max px-4 py-1 text-white rounded-md text-sm mt-4">
+                <Link className="" to="https://calendar.google.com/calendar/u/0/r?tab=mc&pli=1">
                     Schedule
                 </Link>
+
+               
+
+                </div>
             </div>
         </div>
-    
+      <div>Hello</div>
+        <Card sx={{width:1000}}>
+          <CardContent>
+
+          </CardContent>
+        </Card>
     </>
   )
 }
