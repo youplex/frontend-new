c008c77778930dc192a79b990d7eb9de7be8f942
Prajna1999 <gituprajna20@mail.com>
2022-08-19 20:39:28 +0530

    notes

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -1,118 +1,55 @@
-import React from "react";
 
+import Card from "@mui/material/Card";
+
+import { Link } from "react-router-dom";
+
+import { IoEllipsisVerticalSharp } from "react-icons/io5";
+
+import { NotesData } from "../data";
+
+
+const styles = {
+  width: "1125px",
+  backgroundColor: "#EEEFF6",
+  boxShadow:
+    " 0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1);",
+    marginTop:'20px',
+    
+  height:'max-content'
+};
 function Notes() {
   return (
     <>
-      <section className="text-gray-600 ml-52 body-font overflow-hidden">
-        <div className="container  px-5 py-24 mx-auto">
-          <div className="-my-8 divide-y-2 divide-gray-100">
-            <div className="py-8 flex flex-wrap md:flex-nowrap">
-              <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-                <span className="font-semibold title-font text-gray-700">
-                  CATEGORY
-                </span>
-                <span className="mt-1 text-gray-500 text-sm">12 Jun 2019</span>
-              </div>
-              <div className="md:flex-grow">
-                <h2 className="text-2xl font-medium text-gray-900 title-font mb-2">
-                  Bitters hashtag waistcoat fashion axe chia unicorn
-                </h2>
-                <p className="leading-relaxed">
-                  Glossier echo park pug, church-key sartorial biodiesel
-                  vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf
-                  moon party messenger bag selfies, poke vaporware kombucha
-                  lumbersexual pork belly polaroid hoodie portland craft beer.
-                </p>
-                <a className="text-indigo-500 inline-flex items-center mt-4">
-                  Learn More
-                  <svg
-                    className="w-4 h-4 ml-2"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth={2}
-                    fill="none"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  >
-                    <path d="M5 12h14" />
-                    <path d="M12 5l7 7-7 7" />
-                  </svg>
-                </a>
-              </div>
+    {NotesData.map((note,index)=>{
+      return(
+        <>
+        <section className="text-gray-600 ml-52 body-font overflow-hidden" key={note.id}>
+        <Link to="#">
+          <Card sx={{ ...styles }}>
+            {/* <CardMedia component="img" image={item.thumbnail} alt={item.title} /> */}
+            <div className="container flex justify-between px-8 py-4 ">
+              <h1 className="font-semibold text-xl">{note.title}</h1>
+              <button>
+                <IoEllipsisVerticalSharp />
+              </button>
+
+              
             </div>
-            <div className="py-8 flex flex-wrap md:flex-nowrap">
-              <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-                <span className="font-semibold title-font text-gray-700">
-                  CATEGORY
-                </span>
-                <span className="mt-1 text-gray-500 text-sm">12 Jun 2019</span>
+              <div className="flex px-8 pb-4 ">
+
+                <span className="mr-8  font-semibold">{note.timestamp}</span>
+                <p>{note.content}</p>
+                
               </div>
-              <div className="md:flex-grow">
-                <h2 className="text-2xl font-medium text-gray-900 title-font mb-2">
-                  Meditation bushwick direct trade taxidermy shaman
-                </h2>
-                <p className="leading-relaxed">
-                  Glossier echo park pug, church-key sartorial biodiesel
-                  vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf
-                  moon party messenger bag selfies, poke vaporware kombucha
-                  lumbersexual pork belly polaroid hoodie portland craft beer.
-                </p>
-                <a className="text-indigo-500 inline-flex items-center mt-4">
-                  Learn More
-                  <svg
-                    className="w-4 h-4 ml-2"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth={2}
-                    fill="none"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  >
-                    <path d="M5 12h14" />
-                    <path d="M12 5l7 7-7 7" />
-                  </svg>
-                </a>
-              </div>
-            </div>
-            <div className="py-8 flex flex-wrap md:flex-nowrap">
-              <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-                <span className="font-semibold title-font text-gray-700">
-                  CATEGORY
-                </span>
-                <span className="text-sm text-gray-500">12 Jun 2019</span>
-              </div>
-              <div className="md:flex-grow">
-                <h2 className="text-2xl font-medium text-gray-900 title-font mb-2">
-                  Woke master cleanse drinking vinegar salvia
-                </h2>
-                <p className="leading-relaxed">
-                  Glossier echo park pug, church-key sartorial biodiesel
-                  vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf
-                  moon party messenger bag selfies, poke vaporware kombucha
-                  lumbersexual pork belly polaroid hoodie portland craft beer.
-                </p>
-                <a className="text-indigo-500 inline-flex items-center mt-4">
-                  Learn More
-                  <svg
-                    className="w-4 h-4 ml-2"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth={2}
-                    fill="none"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  >
-                    <path d="M5 12h14" />
-                    <path d="M12 5l7 7-7 7" />
-                  </svg>
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
+          </Card>
+        </Link>
       </section>
+        </>
+      )
+    })}
+      
     </>
-  );
+  ); 
 }
 
 export default Notes;
