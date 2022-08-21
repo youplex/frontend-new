2301eff6770d4c4c5e6d920bf71d8df67e413b74
Prajna1999 <gituprajna20@mail.com>
2022-08-21 18:17:37 +0530

    ellipsis menu

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -1,8 +1,12 @@
 import Card from "@mui/material/Card";
 import { Link } from "react-router-dom";
 import { IoEllipsisVerticalSharp } from "react-icons/io5";
-import { Fragment } from "react";
+import { IconContext } from "react-icons";
+import React,{ Fragment,useState } from "react";
+
 import { convertFromRaw, Editor, EditorState } from 'draft-js';
+import { NotesData } from "../data";
+import { positions } from "@mui/system";
 
 const styles = {
   width: "1125px",
@@ -12,6 +16,7 @@ const styles = {
     marginTop:'20px',
     
   height:'max-content'
+  
 };
 
 const convertSecToHMS = (time_in_seconds) => {
@@ -32,35 +37,55 @@ const convertSecToHMS = (time_in_seconds) => {
 }
 
 function Notes({ data = [] }) {
+
+  //ellipsis dropdown menu open/close.
+  const [open,setOpen]=useState(false);
+
   return (
     <>
-    {data?.map((note)=>{
+    {NotesData?.map((note)=>{
       return(
         <Fragment key={note._id}>
-        <section className="text-gray-600 ml-52 body-font overflow-hidden" key={note.id}>
-        <Link to="#">
+        <section className="text-gray-600 ml-52 body-font overflow-hidden " key={note.id}>
+        
           <Card sx={{ ...styles }}>
             {/* <CardMedia component="img" image={item.thumbnail} alt={item.title} /> */}
-            <div className="container flex justify-between px-8 py-2 ">
+            <div className="container  flex justify-between px-4 py-2 ">
               <h1 className="font-semibold text-xl">{note.title}</h1>
-              <button>
-                <IoEllipsisVerticalSharp />
-              </button>
+
+              
+              
+
+                <IoEllipsisVerticalSharp onClick={()=>{setOpen(!open)}} />
+              
+              <div className={`dropdown-menu ${open?'active':'inactive'}`}>
+                <ul>
+
+                <DropdownItem text={"Delete"}/>
+                <DropdownItem text={"Edit"}/>
+                </ul>
+              </div>
+              
+
+
+              
 
               
             </div>
+
               <div className="flex px-8 pb-4 ">
 
                 <span className="mr-8  font-semibold">{convertSecToHMS(note.timestamp)}</span>
-                <div className="readonly-editor" >
+                {/* <div className="readonly-editor" >
                   <Editor 
                     editorState={EditorState.createWithContent(convertFromRaw(JSON.parse(note.content)))} 
                     readOnly={true}
                   />
-                </div>
+                </div> */}
+                <div>{note.content}</div>
               </div>
           </Card>
-        </Link>
+        
       </section>
         </Fragment>
       )
@@ -70,4 +95,14 @@ function Notes({ data = [] }) {
   ); 
 }
 
+//dropdown items.
+
+function DropdownItem(props){
+  return(
+    <li className="list-none dropdown-item">
+      <button >{props.text}</button>
+
+    </li>
+  )
+}
 export default Notes;
