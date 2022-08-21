9a382ed1623366757880bc748f032ab0e51d0228
Prajna1999 <gituprajna20@mail.com>
2022-08-21 19:07:09 +0530

    Link to NavLink

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,47 +1,41 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { SidebarData } from "../data";
 import { HiOutlinePlusCircle } from "react-icons/hi";
 // import * as NoteIcon from "react-icons/fa";
 import { IconContext } from "react-icons";
 // import './sidebar.css';
 
-
 function Sidebar() {
   return (
     <>
       <IconContext.Provider value={{ color: "#fff" }}>
-        <nav className="sidebar-menu fixed top-0 left-0 right-0 flex flex-col h-screen w-20 justify-between items-center py-5 bg-primary ">
+        <nav className="sidebar-menu  fixed top-0 left-0 right-0 flex flex-col h-screen w-20 justify-between items-center py-5 bg-primary ">
           <ul className="sidebar-menu-items ">
             {SidebarData.map((item, index) => {
               return (
-                <li key={index} className=" h-max w-max p-3 mt-5 rounded-full hover-class ">
-                  <Link to={item.path}>
+                <li
+                  key={index}
+                  className="  h-max w-max p-3 mt-5 rounded-full hover-class "
+                >
+                  <NavLink to={item.path}>
                     <IconContext.Provider value={{}}>
-
-                    {item.icon}
+                      {item.icon}
                     </IconContext.Provider>
-                  </Link>
+                  </NavLink>
                 </li>
               );
             })}
           </ul>
 
           {/* Add playlist button */}
-            <div className="  flex  h-max w-max p-3   rounded-full justify-start items-center p-3 ">
-           
-
-          <Link
-            className=" "
-            to="/createplaylist"
-          >
-            <IconContext.Provider value={{style:{color:'white'}}}>
-
-            <HiOutlinePlusCircle size={22} />
-            </IconContext.Provider>
-          </Link>
-            
-            </div>
+          <div className="  flex  h-max w-max p-3   rounded-full justify-start items-center p-3 ">
+            <NavLink to="/createplaylist">
+              <IconContext.Provider value={{ style: { color: "white" } }}>
+                <HiOutlinePlusCircle size={22} />
+              </IconContext.Provider>
+            </NavLink>
+          </div>
         </nav>
       </IconContext.Provider>
     </>
--- src/pages/Scheduling.jsx
+++ src/pages/Scheduling.jsx
@@ -1,22 +1,19 @@
-import { DateTime,Navbar,Sidebar } from "../components";
+import { DateTime, Navbar, Sidebar } from "../components";
 
-import React from 'react'
-
+import React from "react";
 
 export default function Scheduling() {
   return (
     <>
-    <Navbar page='Schedule' />
-    <Sidebar />
-    <div className="ml-52 relative ">
-        
-        <DateTime
-            
-        
-        />
-        
-        <button className="bg-primary  px-2 py-1 absolute top-80 rounded text-white" >Schedule</button>
-    </div>
+      <Navbar page="Schedule" />
+      <Sidebar />
+      <div className="ml-52 relative ">
+        <DateTime />
+
+        <button className="bg-primary  px-2 py-1 absolute top-80 rounded text-white">
+          Schedule
+        </button>
+      </div>
     </>
-  )
+  );
 }
