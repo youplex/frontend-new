44c5192ac7587402d865d7716c0babc924c7cd0a
Rahul Kumar <mrfullstackdev@gmail.com>
2022-08-21 19:08:36 +0530

    Added Loader

---
+++ src/components/Loader.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../loader.css";
+
+function Loader() {
+  return (
+    <>
+      <div id="spinner" className="container">
+        <div className="loading" />
+      </div>
+      ;
+    </>
+  );
+}
+export default Loader;
