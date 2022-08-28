7516d0a4cc50f8b6f5bc36cb58c4134294b86b3a
heyitstay11 <heyitstay011@gmail.com>
2022-08-28 21:31:06 +0530

    removed unused pages added code splitting but it does not wrks due to a bug in react-script

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,23 @@
-import React, { useEffect } from "react";
+import { useEffect, lazy, Suspense } from "react";
 import "./App.css";
 import axios from "axios";
 import { Route, Routes } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { setUser } from "./redux/services/authSlice";
 import {
-  UserHome,
   Dashboard,
   LandingPage,
   CreatePlaylist,
-  PlaylistDashboard,
-  SearchPlaylist,
   Error,
   SinglePlaylist,
-  VideoCourse,
-  VideoNotes,
   Scheduling,
   Privacy
 } from "./pages";
-import { Footer, PrivateRoute } from "./components";
+import { Footer, PrivateRoute, Loader } from "./components";
 import { ToastContainer } from 'react-toastify';
+
+const VideoCourse = lazy(() => import('./pages/VideoCourse'));
+const VideoNotes = lazy(() => import('./pages/VideoNotes'));
 
 function App() {
   const { token } = useSelector((state) => ({ ...state.auth }));
@@ -45,22 +43,21 @@ function App() {
 
   return (
     <>
-      <ToastContainer position="top-center" />
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/home" element={<UserHome />} />
-        <Route path="/createplaylist" element={<CreatePlaylist />} />
-        <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
-        <Route path="/playlistDashboard" element={<PlaylistDashboard />} />
-        <Route  path="/search" element={<PrivateRoute><SearchPlaylist /></PrivateRoute>}/>
-        <Route path="/schedule" element={<PrivateRoute><Scheduling /></PrivateRoute>} />
-        <Route path='/playlist/:playlistId' element={<SinglePlaylist />} />
-        <Route path="/video/:videoId" element={<PrivateRoute><VideoCourse /></PrivateRoute>} />
-        <Route path="/notes" element={<PrivateRoute><VideoNotes /></PrivateRoute>} />
-        <Route path='/privacy' element={<Privacy />}/>
-        <Route path="*" element={<Error />} />
-      </Routes>
-      <Footer />
+    <ToastContainer position="top-center" />
+      <Suspense fallback={<Loader message="Loading your Page ..." />}>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/createplaylist" element={<CreatePlaylist />} />
+            <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
+            <Route path="/schedule" element={<PrivateRoute><Scheduling /></PrivateRoute>} />
+            <Route path='/playlist/:playlistId' element={<SinglePlaylist />} />
+            <Route path="/video/:videoId" element={<PrivateRoute><VideoCourse /></PrivateRoute>} />
+            <Route path="/notes" element={<PrivateRoute><VideoNotes /></PrivateRoute>} />
+            <Route path='/privacy' element={<Privacy />}/>
+            <Route path="*" element={<Error />} />
+          </Routes>
+      </Suspense>
+    <Footer />
     </>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,7 @@
 export { default as Dashboard } from './Dashboard';
 export { default as LandingPage } from './LandingPage';
 export { default as Login } from './Login';
-export { default as UserHome } from './UserHome';
 export { default as CreatePlaylist } from './createPlaylist';
-export { default as PlaylistDashboard } from './PlaylistDashboard';
-export {default as SearchPlaylist } from './SearchPlaylist';
 export {default as Error} from './Error';
 export{default as SinglePlaylist} from './SinglePlaylist';
 export {default as VideoCourse} from './VideoCourse';
